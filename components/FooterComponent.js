22ec8962a201e6df51a1b0499ce5686a8377898e
Mariano Fliri <mfliri@as-one.com.ar>
2019-08-08 17:58:13 -0300

    Add footer responsiveness.

--- components/FooterComponent.js
+++ components/FooterComponent.js
@@ -1,19 +1,19 @@
-export default ()=><div className="footer__container">
+export default () => <div className="footer__container">
   <div className="footer__elements-container">
-    <div className="d-flex justify-content-around">
-      <div>
+    <div className="footer__sign">
       <h2>Invuelto.com</h2>
       <h2>Tu partner de ahorro</h2>
+    </div>
+    <div>
+      <h2 className="mb-4">Seguinos en nuestras redes</h2>
+      <div className='footer__social'>
+        <img src='../static/images/fb.png'></img>
+        <img src='../static/images/in.png'></img>
+        <img src='../static/images/insta.png'></img>
+        <img src='../static/images/twitter.png'></img>
       </div>
-      <div>
-        <h2 className="mb-4">Seguinos en nuestras redes</h2>
-        <div className='w-75 float-right d-flex justify-content-around'>
-          <img src='../static/images/fb.png'></img>
-          <img src='../static/images/in.png'></img>
-          <img src='../static/images/insta.png'></img>
-          <img src='../static/images/twitter.png'></img>
-        </div>
-        </div>
     </div>
   </div>
+  <h3 className="text-color3 text-center">Términos y condiciones</h3>
+
 </div>
