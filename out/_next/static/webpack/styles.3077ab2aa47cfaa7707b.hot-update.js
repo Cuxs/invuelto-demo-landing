e6008f31637559432310b3c64b77246202f11a7a
Mariano Fliri <mfliri@as-one.com.ar>
2019-08-14 15:19:11 -0300

    Deploy demo to gh-pages

--- out/_next/static/webpack/styles.3077ab2aa47cfaa7707b.hot-update.js
+++ out/_next/static/webpack/styles.3077ab2aa47cfaa7707b.hot-update.js
@@ -1,4 +1,4 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["styles"],{
+webpackHotUpdate("styles",{
 
 /***/ "./sass/main.scss":
 /*!************************!*\
@@ -37,5 +37,5 @@
 
 /***/ })
 
-}]);
-//# sourceMappingURL=styles.js.map
+})
+//# sourceMappingURL=styles.3077ab2aa47cfaa7707b.hot-update.js.map
--- out/_next/static/webpack/styles.fadc74f1592b2dfdae9d.hot-update.js
+++ out/_next/static/webpack/styles.fadc74f1592b2dfdae9d.hot-update.js
@@ -1,4 +1,4 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["styles"],{
+webpackHotUpdate("styles",{
 
 /***/ "./sass/main.scss":
 /*!************************!*\
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1565806670214");
+            injectCss(link, link.href.split("?")[0] + "?unix=1565806554769");
           });
       }
     }
@@ -37,5 +37,5 @@
 
 /***/ })
 
-}]);
-//# sourceMappingURL=styles.js.map
+})
+//# sourceMappingURL=styles.fadc74f1592b2dfdae9d.hot-update.js.map
