837bbf17b7c26ec85ae35fa8c118b3821f195a36
Mariano Fliri <mfliri@as-one.com.ar>
2019-09-24 17:53:55 -0300

    Change pig z-index

--- components/Jumbotron.js
+++ components/Jumbotron.js
@@ -6,7 +6,7 @@ export default () => {
       <div className="jumbotron__container row mr-0">
         <div className="jumbotron__elements-container col-md-8 col-lg-6">
           <h3 className="d-block d-md-none jumbotron__spot-title">¡Tu <b>alcancía</b> digital!</h3>
-          <img alt="chachito" className="jumbotron__pig" src="../static/images/pig-neo.png" style={{zIndex:'100'}} />
+          <img alt="chachito" className="jumbotron__pig" src="../static/images/pig-neo.png" style={{zIndex:'10'}} />
           {/* <img alt="moneda" className="jumbotron__coin-1" src="../static/images/coin.png" /> */}
           {/* <img alt="moneda" className="jumbotron__coin-2" src="../static/images/coin.png" /> */}
           <img alt="celular" className="jumbotron__phone" src="../static/images/phone.png" />
