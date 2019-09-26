eddb7b85e90eccbbf5f9b5bd76894bad65e0278e
Mariano Fliri <mfliri@as-one.com.ar>
2019-09-26 14:56:33 -0300

    Fix terminos y condiciones footer.

--- components/FooterComponent.js
+++ components/FooterComponent.js
@@ -11,21 +11,22 @@ export default ({ showImages }) => <div className="footer__container" style={{ h
               <a href="/"><h2 className="text-color3"><b>invuelto</b>.com</h2></a>
             </div>
             <div className="mt-4">
-              <a className="ml-3" target="__blank" rel="noopener noreferrer" href="https://facebook.com/invuelto">{showImages ? <img alt="redes" src='../static/images/fb.png'/> : <div />}</a>
-              <a className="ml-3" target="__blank" rel="noopener noreferrer" href="https://linkedin.com/in/invuelto">{showImages ? <img alt="redes" src='../static/images/in.png'/> : <div />}</a>
-              <a className="ml-3" target="__blank" rel="noopener noreferrer" href="https://instagram.com/invuelto">{showImages ? <img alt="redes" src='../static/images/insta.png'/> : <div />}</a>
-              <a className="ml-3" target="__blank" rel="noopener noreferrer" href="https://twitter.com/invuelto">{showImages ? <img alt="redes" src='../static/images/twitter.png'/> : <div />}</a>
+              <a className="ml-3" target="__blank" rel="noopener noreferrer" href="https://facebook.com/invuelto"><img alt="redes" src='../static/images/fb.png' /></a>
+              <a className="ml-3" target="__blank" rel="noopener noreferrer" href="https://linkedin.com/in/invuelto"><img alt="redes" src='../static/images/in.png' /></a>
+              <a className="ml-3" target="__blank" rel="noopener noreferrer" href="https://instagram.com/invuelto"><img alt="redes" src='../static/images/insta.png' /></a>
+              <a className="ml-3" target="__blank" rel="noopener noreferrer" href="https://twitter.com/invuelto"><img alt="redes" src='../static/images/twitter.png' /></a>
             </div>
           </div>
         </div>
       </div>
       <div className="footer__social-container">
+        <a href="/terminos-y-condiciones"><p className="text-color3 text-center mb-5">Términos y condiciones</p></a>
+        {showImages ? <img alt="decorado" className="footer__circle2" src="../static/images/circle-security2.png" /> : <div />}
       </div>
     </div>
     <img alt="decorado" className="footer__circle2" src="../static/images/circle-security2.png" />
     <img className="deco6" src="/static/images/vueltos/circle-2.svg" alt="decorado" />
   </div>
-  <a href="/terminos-y-condiciones"><p className="text-color3 text-center mb-5">Términos y condiciones</p></a>
-  {showImages ? <img alt="decorado" className="footer__circle2" src="../static/images/circle-security2.png" /> : <div />}
+
 
 </div>
