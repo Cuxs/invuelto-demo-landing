e9b2b73469d743d3efad4981e0cfe67f672f3405
Mariano Fliri <mfliri@as-one.com.ar>
2020-02-28 15:05:08 -0300

    Change area1 items layout. Add facebook posts instance.

--- components/Area1Component.js
+++ components/Area1Component.js
@@ -31,31 +31,25 @@ export default () => <div className="area1__container" id="tus-intereses-primero
     </div>
   </div>
   <div className="d-md-block d-lg-none mt-5 container">
-    <div className="row">
-      <div className="area1__step col">
-        <img alt="alcancia" src="../static/images/celular.svg"></img>
-        <h3><b>Recibí tus vueltos</b> en tu</h3>
-        <h3> alcancía virtual, no más</h3>
-        <h3> caramelos!</h3>
+      <div className="area1__step left">
+        <div><img alt="alcancia" src="../static/images/celular.svg"></img></div>
+        <h3><b>Recibí tus vueltos</b> en tu alcancía virtual, no más caramelos!</h3>
       </div>
-      <div className="area1__step col">
-        <img alt="ahorro" src="../static/images/clock.svg"></img>
-        <h3><b>Programá tu débito</b></h3>
-        <h3>automático</h3>
+      <div className="area1__step right">
+      <div className="decorator--bottom left"/>
+        <h3><b>Programá tu débito</b> automático</h3>
+        <div><img alt="ahorro" src="../static/images/clock.svg"></img></div>
       </div>
-    </div>
-    <div className="row">
-      <div className="area1__step col">
-        <img alt="potenciamos tu ahorro" src="../static/images/reintegro.svg"></img>
-        <h3><b>Obtené reintegros</b></h3>
-        <h3> en tus compras</h3>
+      <div className="area1__step left">
+        <div><img alt="potenciamos tu ahorro" src="../static/images/reintegro.svg"></img></div>
+        <h3><b>Obtené reintegros</b> en tus compras</h3>
+    <div className="decorator--bottom right"/>
       </div>
-      <div className="area1__step col">
-        <img alt="Directo a tu bolsillo" src="../static/images/rendimiento.svg"></img>
-        <h3>Guardá tu plata y </h3>
-        <h3><b>generá intereses</b></h3>
+      <div className="area1__step right">
+      <div className="decorator--bottom left"/>
+        <h3>Guardá tu plata y <b>generá intereses</b></h3>
+        <div><img alt="Directo a tu bolsillo" src="../static/images/rendimiento.svg"></img></div>
       </div>
-    </div>
   </div>
   {/* <PrimaryButton className="mt-5 mb-2" bold='Quiero ahorrar' onClick={()=>window.location.assign('https://app.invuelto.com/register')} /> */}
 </div>
