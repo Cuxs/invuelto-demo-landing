670749ecb241da0ba402865c423ba0de164a547e
Mariano <mariano@invuelto.com>
2020-06-30 15:28:01 -0300

    Fix area2Component styles.

--- components/Area2Component.js
+++ components/Area2Component.js
@@ -9,28 +9,23 @@ export default ({ showImages }) => <div className="area2__container" id="como-fu
   <div className="d-none d-md-none d-lg-flex area2__step-container mb-5">
     <div className="area2__step">
       <img alt="alcancia" src="../static/images/celular.svg"></img>
-      <h3 className="mt-4 mb-5">1. Registrate gratis</h3>
-      <p>Sólo con tu teléfono y </p>
-      <p>DNI, y una contraseña</p>
+      <h3>1. Registrate gratis</h3>
+      <p>Sólo con tu teléfono y DNI, y una contraseña </p>
     </div>
     <div className="area2__step">
       <img alt="ahorro" src="../static/images/invuelto-icon.svg"></img>
-      <h3 className="mt-4 mb-5"> 2. Hacé tu primer ahorro </h3>
+      <h3> 2. Hacé tu primer ahorro </h3>
       <p>Transferí a tu alcancía lo que puedas. </p>
     </div>
     <div className="area2__step">
       <img alt="potenciamos tu ahorro" src="../static/images/ahorro-icon.svg"></img>
-      <h3 className="mt-4">3. Tu plata crece</h3>
-      <h3 className="mb-2"> y se acumula!</h3>
-      <p>Mientras decidís qué hacer con tu plata, </p>
-      <p>la hacemos crecer!</p>
+      <h3>3. Tu plata crece y se acumula!</h3>
+      <p>Mientras decidís qué hacer con tu plata, la hacemos crecer!</p>
     </div>
     <div className="area2__step">
       <img alt="Directo a tu bolsillo" src="../static/images/wallet-icon.svg"></img>
-      <h3 className="mt-4">4. De la alcancía</h3>
-      <h3 className="mb-2">a tu bolsillo</h3>
-      <p> Sacá tu plata cuando quieras </p>
-      <p>y sin costo</p>
+      <h3>4. De la alcancía a tu bolsillo</h3>
+      <p> Sacá tu plata cuando quieras y sin costo </p>
     </div>
   </div>
   <div className="d-md-block d-lg-none">
@@ -47,28 +42,22 @@ export default ({ showImages }) => <div className="area2__container" id="como-fu
       <div className="area2__step">
         <img alt="alcancia" src="../static/images/celular.svg"></img>
         <h3 className="mt-4 mb-5">1. Registrate gratis</h3>
-        <p>Sólo con tu teléfono y </p>
-        <p>DNI, y una contraseña</p>
+        <p>Sólo con tu teléfono y DNI, y una contraseña </p>
       </div>
       <div className="area2__step">
         <img alt="ahorro" src="../static/images/invuelto-icon.svg"></img>
         <h3 className="mt-4 mb-5"> 2. Hacé tu primer ahorro </h3>
-        <p>Vos elegís cómo; vueltos, </p>
-        <p>reintegros, guardar o programar</p>
+        <p>Vos elegís cómo; vueltos, reintegros, guardar o programar </p>
       </div>
       <div className="area2__step">
         <img alt="potenciamos tu ahorro" src="../static/images/ahorro-icon.svg"></img>
-        <h3 className="mt-4">3. Tu plata crece</h3>
-        <h3 className="mb-2"> y se acumula!</h3>
-        <p>Tus ahorros generan </p>
-        <p>ganancias todos los días</p>
+        <h3 className="mt-4">3. Tu plata crece y se acumula!</h3>
+        <p>Tus ahorros generan ganancias todos los días </p>
       </div>
       <div className="area2__step">
         <img alt="Directo a tu bolsillo" src="../static/images/wallet-icon.svg"></img>
-        <h3 className="mt-4">4. De la alcancía</h3>
-        <h3 className="mb-2">a tu bolsillo</h3>
-        <p> Sacá tu plata cuando quieras </p>
-        <p>y sin costo</p>
+        <h3 className="mt-4">4. De la alcancía a tu bolsillo</h3>
+        <p> Sacá tu plata cuando quieras y sin costo </p>
       </div>
     </Carousel>
   </div>
