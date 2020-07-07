6a6930f207721e66eb27b781b3c5d839e4707f67
Mariano <mariano@invuelto.com>
2020-07-07 17:26:58 -0300

    Add review names.

--- components/ReviewComponent/index.jsx
+++ components/ReviewComponent/index.jsx
@@ -10,17 +10,20 @@ const ReviewComponent = () => (
       <div className="review">
         <img src="../static/images/Fabiola.png" />
         <p>Es una forma segura de ahorrar tu plata y que crezca sin el riesgo de perder en el proceso.</p>
+        <small>Fabiola Matucana. Salta</small>
         <img className="stars" src="../static/images/five-stars.svg" />
       </div>
       <div className="review" style={{ marginTop: '150px' }}>
         <img src="../static/images/Lucas.png" />
         <p>Me permite ahorrar sin ningun tipo de esfuerzo ni gestión, con pocos pesos voy incrementando mi alcancía.</p>
+        <small>Lucas Sánchez. Mendoza</small>
         <img className="stars" src="../static/images/five-stars.svg" />
       </div>
       <img src="../static/images/quote-icon.svg" className="quote--icon" />
       <div className="review">
         <img src="../static/images/Segusmundo.png" />
         <p>Permite generar estrategias de ahorro automatizadas. Soluciones digitales para que ahorrar no sea un sacrificio sino algo simple.</p>
+        <small>Segusmundo De Placido. Rio Negro</small>
         <img className="stars" src="../static/images/five-stars.svg" />
       </div>
       <img src="../static/images/quote-background.svg" className="quote--bg" />
@@ -36,16 +39,19 @@ const ReviewComponent = () => (
          <div className="review">
         <img src="../static/images/Fabiola.png" />
         <p>Es una forma segura de ahorrar tu plata y que crezca sin el riesgo de perder en el proceso.</p>
+        <small>Fabiola Matucana. Salta</small>
         <img className="stars" src="../static/images/five-stars.svg" />
       </div>
       <div className="review" style={{ marginTop: '150px' }}>
         <img src="../static/images/Lucas.png" />
         <p>Me permite ahorrar sin ningun tipo de esfuerzo ni gestión, con pocos pesos voy incrementando mi alcancía.</p>
+        <small>Lucas Sánchez. Mendoza</small>
         <img className="stars" src="../static/images/five-stars.svg" />
       </div>
       <div className="review">
         <img src="../static/images/Segusmundo.png" />
         <p>Permite generar estrategias de ahorro automatizadas. Soluciones digitales para que ahorrar no sea un sacrificio sino algo simple.</p>
+        <small>Segusmundo De Placido. Rio Negro</small>
         <img className="stars" src="../static/images/five-stars.svg" />
       </div>
       </Carousel>
