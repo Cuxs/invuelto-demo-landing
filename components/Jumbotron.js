e8f7ee3c809c330221cebaaf42c4503edd386507
Mariano <mariano@invuelto.com>
2020-08-21 13:30:51 -0300

    Add mi-vuelto button.

--- components/Jumbotron.js
+++ components/Jumbotron.js
@@ -13,9 +13,11 @@ export default () => {
           <h3 className="d-none d-sm-none d-md-block jumbotron__spot-title">¡Tu alcancía ahora en el celu!</h3>
         </div>
         <div className="jumbotron__title col-lg-5 col-md-12 pr-0 mb-3">
-          <h2><b>Una alcancía inteligente donde juntar plata es muuucho más fácil. </b></h2>
-          <p className="mb-5">Descubrila</p>
-          <PrimaryButton style={{width:'260px !important'}} className="mt-0" bold="registrarme" onClick={()=>window.location.assign('https://app.invuelto.com/register')} />
+          <h2 className="d-none d-sm-none d-md-block"><b>Una alcancía inteligente donde juntar plata es muuucho más fácil. </b></h2>
+          {/* <p className="mb-5">Descubrila</p> */}
+          <h2 className="text-primary"><b>¿Te entregaron un vuelto?</b></h2>
+
+          <PrimaryButton style={{width:'260px !important'}} className="mt-0" bold="Guardalo acá" onClick={()=>window.location.assign('https://app.invuelto.com/mi-vuelto')} />
         </div>
 
         <div className="jumbotron__circle">
